f703907f1f4cadaebbd6a551647fbc4fed76905a
kkkkkkk96 <d8362721@gmail.com>
2026-01-10 03:50:32 +0500

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,57 +1,11 @@
-// Генерирует случайное число (целое) в заданном диапазоне
-const getRandomInteger = (minValue, maxValue) => {
-  const min = Math.ceil(Math.min(minValue, maxValue));
-  const max = Math.floor(Math.max(minValue, maxValue));
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
-// Проверяет, является ли клавиша Esc
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-const showAlert = (message) => {
-  const alertContainerElement = document.createElement('div');
-  alertContainerElement.style.zIndex = '100';
-  alertContainerElement.style.position = 'fixed';
-  alertContainerElement.style.left = '0';
-  alertContainerElement.style.top = '0';
-  alertContainerElement.style.right = '0';
-  alertContainerElement.style.padding = '10px 3px';
-  alertContainerElement.style.fontSize = '30px';
-  alertContainerElement.style.textAlign = 'center';
-  alertContainerElement.style.backgroundColor = 'red';
-  alertContainerElement.textContent = message;
-
-  document.body.append(alertContainerElement);
-
-  setTimeout(() => {
-    alertContainerElement.remove();
-  }, 5000);
-};
-
-// Функция для устранения дребезга
-const debounce = (callback, timeoutDelay = 500) => {
+function debounce (callback, timeoutDelay = 500) {
   let timeoutId;
-
-  return (...rest) => {
+  return function (...rest) {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
-};
+}
 
-// Функция для пропуска кадров
-const throttle = (callback, delayBetweenFrames) => {
-  let lastTime = 0;
-
-  return (...rest) => {
-    const now = new Date();
-
-    if (now - lastTime >= delayBetweenFrames) {
-      callback.apply(this, rest);
-      lastTime = now;
-    }
-  };
-};
-
-export { getRandomInteger, getRandomArrayElement, isEscapeKey, showAlert, debounce, throttle };
+export { debounce, isEscapeKey };
