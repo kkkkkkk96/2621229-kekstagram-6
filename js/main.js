f05dccb052e4a5be8de60f6b5baf8d3ababdd4b4
kkkkkkk96 <d8362721@gmail.com>
2026-01-10 03:45:00 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,21 @@
+import { renderThumbnails, initThumbnailsHandlers } from './pictures.js';
+import { initForm, setFormSubmit } from './form.js';
+import { initScaleEditor } from './scale-photo.js';
 import { getData } from './api.js';
-import { renderThumbnails } from './thumbnails.js';
+import { showAlert } from './message.js';
 import { initFilters } from './filters.js';
-import './form.js';
+import { debounce } from './util.js';
 
-const picturesContainerElement = document.querySelector('.pictures');
-
-// Функция для показа сообщения об ошибке
-const showErrorMessage = (message) => {
-  const errorTemplateElement = document.querySelector('#error').content.querySelector('.error');
-  const errorElement = errorTemplateElement.cloneNode(true);
-  const errorTitleElement = errorElement.querySelector('.error__title');
-  const errorButtonElement = errorElement.querySelector('.error__button');
-
-  errorTitleElement.textContent = message;
-  errorButtonElement.textContent = 'Попробовать снова';
-
-  errorButtonElement.addEventListener('click', () => {
-    document.location.reload();
+getData()
+  .then((photos) => {
+    renderThumbnails(photos);
+    initFilters(photos, debounce(renderThumbnails));
+    initThumbnailsHandlers();
+  })
+  .catch((err) => {
+    showAlert(err.message);
   });
 
-  document.body.appendChild(errorElement);
-};
-
-// Загрузка данных с сервера и отрисовка миниатюр
-const loadAndRenderPhotos = async () => {
-  try {
-    const photosArray = await getData();
-    initFilters(photosArray);
-    renderThumbnails(photosArray, picturesContainerElement);
-  } catch (error) {
-    showErrorMessage(error.message);
-  }
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-  loadAndRenderPhotos();
-});
+setFormSubmit();
+initForm();
+initScaleEditor();
